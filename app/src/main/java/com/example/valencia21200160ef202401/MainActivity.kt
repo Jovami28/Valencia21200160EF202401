6268e8a0b53540726d1e466056971b859a4b7672
JOVAMI2805 <joseph.valencia.m@gmail.com>
2024-07-14 12:52:50 -0500

    Caso 01

--- app/src/main/java/com/example/valencia21200160ef202401/MainActivity.kt
+++ app/src/main/java/com/example/valencia21200160ef202401/MainActivity.kt
@@ -1,47 +1,77 @@
 package com.example.valencia21200160ef202401
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.valencia21200160ef202401.ui.theme.Valencia21200160EF202401Theme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Valencia21200160EF202401Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        val tvNombreEquipo: TextView = findViewById(R.id.tvNombreEquipo)
+        val tvURL: TextView = findViewById(R.id.tvURL)
+        val btSave: Button = findViewById(R.id.btSave)
+        val db = FirebaseFirestore.getInstance()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Valencia21200160EF202401Theme {
-        Greeting("Android")
+        btSave.setOnClickListener {
+            val NombreEquipo = tvNombreEquipo.text.toString()
+            val URL = tvURL.text.toString()
+
+            db.collection("Equipos")
+            .addSnapshotListener{snapshots, e->
+                if(e!=null){
+                    Log.e("ERROR FIREBASE","Ocurrió un error");
+                    return@addSnapshotListener
+                }
+
+                for(dc in snapshots!!.documentChanges){
+                    when(dc.type){
+                        DocumentChange.Type.ADDED, DocumentChange.Type.MODIFIED ->
+                        {
+                            tvNombreEquipo.text = dc.document.data["NombreEquipo"].toString()
+                            tvURL.text = dc.document.data["ImagenEquipo"].toString()
+
+                            Snackbar.make(
+                                findViewById(android.R.id.content)
+                                , "Se agregó/modificó un documento"
+                                , Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+
+                        DocumentChange.Type.REMOVED -> {
+                            Snackbar.make(
+                                findViewById(android.R.id.content)
+                                , "Se eliminó un documento"
+                                , Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
+
+
+
+            }
+
+
+
+
+
+
+        }
     }
 }
